9dc1ee4ba7f67b0b422d105e02d42b7bf6e7fdbd
AltechInferno <altechinferno@gmail.com>
2024-03-07 12:57:47 +0000

    task0

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -4,22 +4,22 @@
 
 
 def island_perimeter(grid):
-
+    """Computes perimeter of an island with no lakes
+    """
     perimeter = 0
-
-    if not isinstance(grid, list):
+    if type(grid) != list:
         return 0
-
-    rows, cols = len(grid), len(grid[0])
-
-    for i in range(rows):
-        for j in range(cols):
-            if grid[i][j] == 1:
-                perimeter += 4
-
-                if i > 0 and grid[i - 1][j] == 1:
-                    perimeter -= 2
-                if j > 0 and grid[i][j - 1] == 1:
-                    perimeter -= 2
-
+    n = len(grid)
+    for l, row in enumerate(grid):
+        m = len(row)
+        for w, cell in enumerate(row):
+            if cell == 0:
+                continue
+            borders = (
+                l == 0 or (len(grid[l - 1]) > w and grid[l - 1][w] == 0),
+                w == m - 1 or (m > w + 1 and row[w + 1] == 0),
+                l == n - 1 or (len(grid[l + 1]) > w and grid[l + 1][w] == 0),
+                w == 0 or row[w - 1] == 0,
+            )
+            perimeter += sum(borders)
     return perimeter
