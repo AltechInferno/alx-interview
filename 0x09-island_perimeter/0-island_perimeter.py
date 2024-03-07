a3a78aaadc7dd9f0a1408705052244845c04345f
AltechInferno <altechinferno@gmail.com>
2024-03-07 12:40:04 +0000

    task0

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -2,22 +2,22 @@
 """Island perimeter computing
 """
 
-
 def island_perimeter(grid):
- """Computes perimeter of an island with no lakes
-    """
     perimeter = 0
+    if not isinstance(grid, list):
+        return 0
+
     rows, cols = len(grid), len(grid[0])
 
-    for row in range(rows):
-        for col in range(cols):
-            if grid[row][col] == 1:
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == 1:
                 perimeter += 4
 
-                # Check if there's land in neighboring cells
-                if row > 0 and grid[row - 1][col] == 1:
+                if i > 0 and grid[i - 1][j] == 1:
                     perimeter -= 2
-                if col > 0 and grid[row][col - 1] == 1:
+                if j > 0 and grid[i][j - 1] == 1:
                     perimeter -= 2
 
     return perimeter
+
